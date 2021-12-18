73fdd6f5ed8011627c4aa633bc9526fca0932cf8
Andrey Trusov <trusov@haulmont.com>
2021-12-18 22:40:50 +0400

    some action fix1

--- src/main/java/ru/papont/library/dto/BookDetailsDto.java
+++ src/main/java/ru/papont/library/dto/BookDetailsDto.java
@@ -2,12 +2,15 @@ package ru.papont.library.dto;
 
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class BookDetailsDto extends BookSmallDto {
     private List<BookHistoryDto> bookHistories;
 }
+
